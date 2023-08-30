2123470eb095474a01d618bf524a86d26df7d7c4
Juan <juanquibar@gmail.com>
2023-08-30 09:37:37 -0300

    listo con estilos sin eliminar prisma

--- app/page.js
+++ app/page.js
@@ -1,51 +1,104 @@
 "use client"
+import {useState, useEffect} from 'react'
 import useSWR from "swr"
 import axios from 'axios'
 import Spinner from '../components/spinner'
 import Departamento from "../components/Departamento"
 
-/* async function getData() {
 
-  const res = await fetch('http://localhost:3000/api');
 
-  if (!res.ok) {
-    throw new Error('Error en el fetch')
-  }
- 
-  return res.json()
-} */
+export default function Home (){
 
-// Lisado index más  ID de departamentos
-// 0 Maipú 1
-// 1 San Rafael 2
-// 2 Tunuyán 3
-// 3 Lavalle 4
-// 4 La Paz 5
-// 5 San Carlos 6
-// 6 Santa Rosa 7
+  const [widthCards, setWidthCards] = useState(0)
+  const [widthSlider, setWidthSlider] = useState(0)
+  const [marginLeft, setMarginLeft] = useState(0)
+  const [elementsToShow, setElementsToShow] = useState(0)
+  const [prueba, setPrueba] = useState(true)
 
-export default function Home() {
-  
   const fetcher = () => axios('/api').then(datos => datos.data)
-  
-  const { data, error, isLoading } = useSWR('/api', fetcher, { refreshInterval: 1000 })
- 
-  if (error) console.log(error)
-  if (isLoading) return <div className='flex justify-center pt-7 w-screen px-4 gap-3'> <Spinner /> </div>
-  
-  return (
-  
-    <div className='flex w-screen px-4 gap-3'>
-    
-      {data && data.length ? data.map(departamento => (
-        <div className="w-[300px] " key={departamento.id}>
-          <Departamento
-            departamento={departamento} 
-          />
-        </div>  
-      )) : null
-      }
 
-    </div>
+  const { data, error, isLoading } = useSWR('/api', fetcher, { refreshInterval: 1000 })
+
+  error ? console.log(error) : ''
+  isLoading ?   <Spinner />  : ''
+
+  useEffect(() => {
+      let widthContainer = document.getElementById('sliderContainer').clientWidth;
+      
+      let slider = document.getElementById('slider')
+      
+      let toShow =    widthContainer > 1280 ? 7 :  
+                      widthContainer >= 1025 ? 5:
+                      widthContainer >= 768 ? 3: 
+                      widthContainer < 768 ? 2: 
+                              '' 
+      setElementsToShow(toShow)
+                          
+      setWidthCards((widthContainer)/(elementsToShow))
+      setWidthSlider(7*widthCards + "px")
+
+  }, [widthCards]);
+
+
+  return (
+      <section>
+          <div className="">
+
+              {/* CODIGO DE BOTONES DE DESPLAZAMIENTO LATERAL */}
+              <div className="flex justify-end pr-3 -mb-2 xl:hidden">
+
+                  <button 
+                      onClick={() => marginLeft !== 0 ? setMarginLeft((marginLeft)+widthCards) : ''} 
+
+                      className={marginLeft == 0 ? 'text-slate-100' : 'text-[#0096FF]'} 
+                  >
+
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 lg:w-10 lg:h-10">
+                      <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-4.28 9.22a.75.75 0 000 1.06l3 3a.75.75 0 101.06-1.06l-1.72-1.72h5.69a.75.75 0 000-1.5h-5.69l1.72-1.72a.75.75 0 00-1.06-1.06l-3 3z" clipRule="evenodd" />
+                      </svg>
+                  </button>
+                 
+                  <button 
+                      onClick={() => marginLeft !== -widthCards*(7-elementsToShow) ? setMarginLeft((marginLeft)-widthCards) : ''} 
+
+                      className={marginLeft !== -widthCards*(7-elementsToShow) ? 'text-[#0096FF]' : 'text-slate-100'}
+                  >
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 lg:w-10 lg:h-10">
+                      <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" clipRule="evenodd" />
+                      </svg>
+
+                  </button>
+                     
+              </div>
+
+              <div id="sliderContainer" className="w-full overflow-hidden ">
+                  
+                  <ul id="slider" className="slider flex w-full transition-margin duration-[900ms]">
+
+                      <style jsx>{`
+                          .slider{
+                          width: ${widthSlider};
+                          margin-left: ${marginLeft+'px'};
+                          }
+                          .ancho{
+                              width: ${widthCards+'px'};
+                              }
+                      `}</style>
+
+                      {data && data.length ? data.map(departamento => (
+                          <li id="lista" className=" ancho p-3" key={departamento.id}>
+                          <Departamento
+                              departamento={departamento} 
+                          />
+                          </li>  
+                      )) : null
+                      }
+
+                  </ul>
+
+              </div>
+
+          </div>
+      </section>
   )
 }
--- components/Departamento.js
+++ components/Departamento.js
@@ -7,22 +7,41 @@ const Departamento = ({departamento}) => {
     
   return (
     
-    <div className="bg-slate-100" >
-        <h2 className="text-center text-lg text-red-100 bg-slate-800">{departamento.nombre}</h2>
-        <p className="text-xs">Mesas: {departamento.mesas} - Escrutadas: {departamento.escrutadas} </p>
-        <Image width={150} height={150} src={`/assets/${candidatos[0].foto}.png`} alt="Foto candidato" />
-        <div className="text-xs bg-red-300">
-            <p>{candidatos[0].nombre} <span className="text-base font-bold">{candidatos[0].porcentaje}% </span></p>
-            <p>{candidatos[0].fuerza} </p> 
+    <div className="bg-[#F5F4F4]" >
+
+        <div className="h-5 bg-[#656565] flex justify-center items-center ">
+            <p className="text-[10px] text-white font-bold ">Mesas: <span className="font-light">{departamento.mesas}</span> - Escrutadas: <span className="font-light">{departamento.escrutadas}</span> </p>
+        </div>
+
+        <div className="h-7 w-full bg-white flex justify-center items-center">
+            <h2 className="text-[15px] text-[#0096FF] uppercase font-black">{departamento.nombre}</h2>
+        </div>
+
+        <div className="flex justify-center">
+            <Image width={200} height={200} src={`/assets/${candidatos[0].foto}.png`} alt="Foto candidato" />
+        </div>
+
+
+        <div className="bg-white h-7 flex justify-center items-center">
+            <p className="text-[#0096FF] text-sm font-normal">{candidatos[0].nombre} <span className="font-black">{candidatos[0].porcentaje}% </span></p>     
+        </div>
+        <div className="bg-[#00549E] h-3.5 flex justify-center items-center">
+            <p className="text-white text-[8px] font-bold">{candidatos[0].fuerza} </p>     
         </div>
 
         <div >
 
             {candidatos?.map((perdedor, i) => (
                 i>0?
-                <div key={perdedor.id} className="text-xs py-2 border border-black">
-                    <p>{perdedor.nombre} - {perdedor.porcentaje }%</p>
-                    <p>{perdedor.fuerza}</p>
+                <div key={perdedor.id}>
+                    <div className="flex flex-col">
+                        <div className="flex justify-center items-center bg-white h-6 pt-1">
+                            <p className="text-[10px] text-[#656565] ">{perdedor.nombre} <span className="font-black">{perdedor.porcentaje}%</span></p>
+                        </div>
+                        <div className="h-3 bg-[#656565] flex justify-center items-center">
+                            <p className="text-white text-[6px] ">{perdedor.fuerza}</p>
+                        </div>
+                    </div>
                 </div>
                 : null
         
